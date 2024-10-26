a490c163a9f1e02b039bc42ee79962df067dfd44
karlach-a <karlach.argueta@gmail.com>
2024-10-26 17:14:55 -0600

    cambio de nombre de rama

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
 {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
     </div>
     <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]': 'fixed left-[-100%]'}>
-    <h1 className='w-full text-3xl font-bold text-[#04dbde] m-4'> Code Education</h1>
+    <h1 className='w-full text-3xl font-bold text-[#2eadaf] m-4'> Code Education</h1>
         <ul className='uppercase p-4 '>
         <li className='p-4 border-b border-gray-600'>Inicio</li>
         <li className='p-4 border-b border-gray-600'>Usuarios</li>
