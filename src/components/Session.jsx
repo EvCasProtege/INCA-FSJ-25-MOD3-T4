f3b27e4cc1d546f7d97a0be74f1eb3ddcb258758
kevin-rodriguez-dev <kevinrodrihid2003@gmail.com>
2024-10-26 23:47:10 -0600

    fix: Agregando Logo al login

--- src/components/Session.jsx
+++ src/components/Session.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { LoginForm } from "./auth/LoginForm";
 import { RegisterForm } from "./auth/RegisterForm";
 import { useNavigate } from "react-router-dom";
+import codeEducationLogo from "../assets/logo/codeeducation.png";
 
 export const Session = () => {
     const [typeForm, setTypeForm] = useState("login");
@@ -10,9 +11,11 @@ export const Session = () => {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-900">
             <div className="max-w-md w-full bg-gray-800 rounded-lg shadow-lg p-8">
-                <h2 className="text-3xl font-bold text-center text-teal-400 mb-8">
+                <img src={codeEducationLogo} alt="code-education-logo" className="mx-auto w-20 h-auto" />
+
+                {/* <h2 className="text-3xl font-bold text-center text-teal-400 mb-8">
                     {typeForm === "login" ? "Iniciar Sesión" : "Registro"}
-                </h2>
+                </h2> */}
                 
                 <div className="space-y-6">
                     {typeForm === "login" ? (
