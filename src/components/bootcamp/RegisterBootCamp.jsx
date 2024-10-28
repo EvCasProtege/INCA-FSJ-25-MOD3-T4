abdbbcc52a89b852777b9d35c177967e9dbcdb0e
EvCasProtege <ever_castro@protegeseguros.com.sv>
2024-10-28 16:22:59 -0600

    fixed register

--- src/components/bootcamp/RegisterBootCamp.jsx
+++ src/components/bootcamp/RegisterBootCamp.jsx
@@ -8,12 +8,12 @@ import { UserComponent } from '../UserComponent';
 import { Footer } from '../landing/Footer';
 
 const RegisterBootCamp = () => {
-
   const {
     register,
     handleSubmit,
     formState: { errors },
-} = useForm();
+    setValue,
+  } = useForm();
 
   const [data, setData] = useState({
     name: '',
@@ -22,7 +22,6 @@ const RegisterBootCamp = () => {
   });
 
   const navigate = useNavigate();
-
   const [error, setError] = useState('');
 
   const handleChange = (e) => {
@@ -31,6 +30,7 @@ const RegisterBootCamp = () => {
       ...prevData,
       [name]: value,
     }));
+    setValue(name, value);
   };
 
   const handleTechnologyChange = (index, value) => {
@@ -40,6 +40,7 @@ const RegisterBootCamp = () => {
       ...prevData,
       technologies: newTechnologies,
     }));
+    setValue(`technologies[${index}]`, value);
   };
 
   const addTechnology = () => {
@@ -57,11 +58,15 @@ const RegisterBootCamp = () => {
     }));
   };
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
+  const onSubmit = async (formData) => {
     setError('');
+    if (data.technologies.length === 0 || data.technologies.some(tech => !tech)) {
+      setError('Debes agregar al menos una tecnología.');
+      return;
+    }
     try {
       await bootcampService.createBootcamp(data);
+
       navigate('/bootcamps');
     } catch (err) {
       setError(err.message);
@@ -83,21 +88,20 @@ const RegisterBootCamp = () => {
               name="name"
               placeholder='Ej. Desarrollo Web con ReactJS'
               value={data.name}
-              {...register("name", { required: "El nombre del bootcamp requerido." })}
+              {...register("name", { required: "El nombre del bootcamp es requerido." })}
               onChange={handleChange}
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-
             />
             {errors.name && <p className="mt-2 text-sm text-red-600 text-center">{errors.name.message}</p>}
           </div>
-          <div className='mb-5' >
+          <div className='mb-5'>
             <label className="block text-sm font-medium text-gray-300">Descripción:</label>
             <textarea
               id='description'
               name="description"
               placeholder='Descripción del Bootcamp'
               value={data.description}
-              {...register("description", { required: "Debes agregar una descripción para el bootcamp" })}
+              {...register("description", { required: "Debes agregar una descripción para el bootcamp." })}
               onChange={handleChange}
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
@@ -106,25 +110,25 @@ const RegisterBootCamp = () => {
           <div className="mb-5">
             <label className="block mb-5 text-sm font-medium text-gray-300">Tecnologías:</label>
             {data.technologies.map((tech, index) => (
-                <div key={index} className="flex items-center mb-4">
-                    <input
-                        id='technology'
-                        type="text"
-                        placeholder='Ej. JavaScript'
-                        value={tech}
-                        {...register("technology", { required: "Debes agregar al menos una tecnología." })}
-                        onChange={(e) => handleTechnologyChange(index, e.target.value)}
-                        className="flex-grow p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    />
-                    <button type="button" onClick={() => removeTechnology(index)} className="ml-2 text-red-500 hover:text-red-700" aria-label="Eliminar tecnología">
-                        <AiOutlineDelete size={20} />
-                    </button>
-                </div>
+              <div key={index} className="flex items-center mb-4">
+                <input
+                  id='technology'
+                  type="text"
+                  placeholder='Ej. JavaScript'
+                  value={tech}
+                  {...register(`technologies[${index}]`, { required: "Debes agregar al menos una tecnología." })}
+                  onChange={(e) => handleTechnologyChange(index, e.target.value)}
+                  className="flex-grow p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+                <button type="button" onClick={() => removeTechnology(index)} className="ml-2 text-red-500 hover:text-red-700" aria-label="Eliminar tecnología">
+                  <AiOutlineDelete size={20} />
+                </button>
+              </div>
             ))}
             <button type="button" className="mt-2 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#2eadaf]" onClick={addTechnology}>
-                Añadir tecnología
+              Añadir tecnología
             </button>
-            {errors.technology && <p className="mt-2 text-sm text-red-600 text-center">{errors.technology.message}</p>}
+            {errors.technologies && <p className="mt-2 text-sm text-red-600 text-center">{errors.technologies.message}</p>}
           </div>
           <div>
             {error && <div className="mt-2 text-sm text-red-600 text-center">{error}</div>}
